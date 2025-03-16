046b9151e5094673ebe6577d9947e88577bdc2c9
JL Garcia <jl.garcia@gmail.com>
2025-03-16 15:28:41 -0400

    Update NewsLetters.tsx

--- components/Home/NewsLetters/NewsLetters.tsx
+++ components/Home/NewsLetters/NewsLetters.tsx
@@ -6,7 +6,7 @@ const NewsLetters = () => {
     <div className='bg-black pt-16 pb-16 flex items-center justify-center w-full flex-col'>
         <Mail className='w-16 h-16 text-white' />
         <h1 className='text-lg sm:text-xl md:text-2xl text-white font-semibold mt-10 tracking-widest'>Your Journey Starts Here</h1>
-        <p className='mt-3 text-white textxs sm:text-sm'>Sign up and we'll send you the best deals!</p>
+        <p className='mt-3 text-white text-xs sm:text-sm'>Sign up and we will send you the best deals!</p>
         <div className='w-full'>
             <input type="text" className='px-6 py-3.5 bg-white mt-8 w-[95%] sm:w-[50%] md:w-[60%] lg:w-[40%] block mx-auto rounded-lg outline-none' placeholder='Email' />
             <button  className='px-6 py-3.5 hover:bg-blue-950 text-white bg-blue-900 transition-all duration-200 mt-3 w-[95%] sm:w-[50%] md:w-[60%] lg:w-[40%] block mx-auto rounded-lg outline-none'>Subscribe</button>
